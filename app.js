68f9f249d9d8691205c9b3438a3845de3753b0c3
ajalaba <ajalaba@deakin.edu.au>
2020-07-13 14:35:32 +1000

    ADD: New send command file

--- app.js
+++ app.js
@@ -16,3 +16,8 @@ $('#add-device').on('click', function() {
     localStorage.setItem('devices', JSON.stringify(devices));
     location.href = 'device-list.html';
 });
+
+$('#send-command').on('click', function() {
+    const command = $('#command').val();
+    console.log(`command is: ${command}`);
+    });
