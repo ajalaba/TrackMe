0bda260df75979a935eae9729d24115a0e008341
ajalaba <ajalaba@deakin.edu.au>
2020-07-17 18:35:31 +1000

    UPDATE: Change server code

--- web/server.js
+++ web/server.js
@@ -14,13 +14,13 @@ app.get('/', function (req, res) {
     });
 app.get('/register-device', (req, res) => {
     res.sendFile(`${base}/register-device.html`);
-        });
+    });
 app.get('/send-command', (req, res) => {
-        res.sendFile(`${base}/send-command.html`);
-        });
+    res.sendFile(`${base}/send-command.html`);
+    });
 app.get('/about', (req, res) => {
-        res.sendFile(`${base}/about-me.html`);
-        });
+    res.sendFile(`${base}/about-me.html`);
+    });
 app.get('*', (req, res) => {
     res.sendFile(`${base}/404.html`);
     });
